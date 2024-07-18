40a57f3b45725d781dcc25c2f0b62f119b896bfd
guillemgalindo <141041271+guillemgalindo@users.noreply.github.com>
2024-07-18 12:03:33 +0000

    :art: Format Python code with psf/black

--- main.py
+++ main.py
@@ -2,4 +2,7 @@ import os, sys
 from pprint import pprint
 import re
 
-test_string = 'This is a very long string with the wrong name format to see what black will do. It is also using single quotes instead of double quotes.'
+test_string = (
+    "This is a very long string with the wrong name format to see what black will do. It is also using single quotes"
+    " instead of double quotes."
+)
